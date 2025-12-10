3c7aaa537787fea55a78261814430f5f7daf8554
Cyrex23 <45338293+Cyrex24@users.noreply.github.com>
2025-12-10 13:14:13 +0000

    gg

--- src/pt/iscte/poo/game/GameEngine.java
+++ src/pt/iscte/poo/game/GameEngine.java
@@ -15,7 +15,7 @@ import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.HighScore;
 import pt.iscte.poo.utils.SoundManager;
 
-//Exercicio de POO: 
+//Exercicio de POO: Olá
 public class GameEngine implements Observer {
 
 	private Map<String,Room> rooms;
